c619626073f8a8f64bb95a18d2dbc97c430b3c6f
Michaeldiaz <michaeldiaz311@gmail.com>
2023-08-01 10:20:34 -0500

    Agregra nuevo codigo

---
+++ Script.js
@@ -0,0 +1,27 @@
+// Clase Personas
+class Personas {
+  constructor(nombre, apellido, edad, cedula) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.cedula = cedula;
+  }
+}
+
+// Clase Animales
+class Animales {
+  constructor(nombre, genero, raza) {
+    this.nombre = nombre;
+    this.genero = genero;
+    this.raza = raza;
+  }
+}
+
+// Clase Veterinaria
+class Veterinaria {
+  constructor(nombre, direccion, telefono) {
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.telefono = telefono;
+  }
+}
